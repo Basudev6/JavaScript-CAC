dfb6eb627d20c187fcb83bc0eb12e20461f7d0fb
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-15 08:01:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello")   we are using node js, not browser
+
+let name = "Basudev"
+let number = 20
+let isLoggedIn = true
+
+// number => 2 to the power of 53
+// bigInt
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "ram")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
