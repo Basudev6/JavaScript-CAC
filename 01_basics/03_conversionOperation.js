d43910bc19c54028d1bd5f6bf1b8b87f9e97e984
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-15 09:06:27 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,8 +17,32 @@ let valueIntNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 // "" => false
 // "ram" => true
+
+
+// **********************Operations*******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+ 
+let str1 = "hi"
+let str2 = " basu"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
