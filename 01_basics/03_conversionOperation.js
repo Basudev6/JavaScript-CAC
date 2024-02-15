6d50b8421d224b3cf7b85f997a6bb549e581f137
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-15 08:29:51 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "50aa"
+// let score = null
+// let score = undefined
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueIntNumber = Number(score)
+// console.log(typeof valueIntNumber)
+// console.log(valueIntNumber)
+
+// "33" => 33
+// "33abcd" => NaN
+// true => 1
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "ram" => true
