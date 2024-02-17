ba67ea95d8584140dd7c5ef975cac03e865a5b3b
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-17 12:48:27 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+// array
+
+const myArr = [1,5,6,2,3,8]
+const myHeros = ["shaktiman","superman"]
+
+const myArr2 = new Array(1,3,5,6)
+
+// console.log(myArr[2])
+
+// array methods
+
+// myArr.push(10)
+// myArr.pop()
+
+// myArr.unshift(4)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(7))
+// console.log(myArr.indexOf(5))
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+// slice, splice
+
+console.log("A",myArr)
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("B",myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("C",myArr)
