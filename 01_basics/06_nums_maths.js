e9f8a2123741e5c8b824b0d36c78720e92d6c139
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-17 08:19:32 +0000

    number and maths

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,31 @@
+const score = 900;
+// console.log(score)
+
+const balance = new Number(200)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+const otherNumber = 129.8776
+// console.log(otherNumber.toPrecision(5))
+
+const number = 10000000
+// console.log(number.toLocaleString('en-IN'))
+
+// *********************** Maths ********************
+
+// console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.round(4.68))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(9,2,1,5))
+// console.log(Math.max(9,8,3,1))
+
+console.log(Math.random())
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
