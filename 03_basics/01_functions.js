fad70011e97b28c49caf03f1a8eeccc19a6d1374
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-20 09:34:54 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,4 +19,33 @@ const result = sum(5,4)
 function loginUserMessage(username="ram"){
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Basu"))
+// console.log(loginUserMessage("Basu"))
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(500,200,100,50,40))
+
+const user = {
+    username: "Basudev",
+    age : "23"
+}
+
+function handleObject(anybody){
+    console.log(`Username is ${anybody.username} and age is ${anybody.age}`)
+}
+// handleObject(user)
+handleObject({
+    username:"ram",
+    age: 20
+})
+
+const array = [100,900,150,200,400]
+function returnSecondArray(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondArray(array))
+
+console.log(returnSecondArray([10,20,40,50]))
