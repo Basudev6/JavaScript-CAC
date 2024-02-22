60d54e825a42e5977cfc688436569a4823b7f48d
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-22 14:46:39 +0000

    do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,12 @@
+// let i = 0
+// while(i<20){
+//     console.log(`The value of i is ${i} `)
+//     i++
+// }
+
+
+let score = 1
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score<=10)
