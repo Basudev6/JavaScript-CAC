db95503491f3f2373e084c23f1b408d33d09bb5d
Basudev Neupane <96032670+Basudev6@users.noreply.github.com>
2024-02-23 07:13:43 +0000

    for in

---
+++ 05_iteration/four.js
@@ -0,0 +1,16 @@
+const myObj = {
+    js:"javascript",
+    cpp:"c++",
+    py:"python",
+    rb:"ruby"
+}
+
+// for in loop
+for(const key in myObj){
+    // console.log(`${key} : ${myObj[key]}`)
+}
+
+const programming = ["js","python","cpp","java"]
+for(const key in programming){
+    console.log(programming[key])
+}
